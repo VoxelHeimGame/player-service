cfe69830cb5a3aabf206b808abe3731c0195ac46
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-12-03 23:48:32 +0100

    chore: add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get('/', () => 'Player Service is running').listen(3003)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`👤 Player Service is running at ${app.server?.hostname}:${app.server?.port}`)
